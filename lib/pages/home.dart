87c7ed0cc00c18f010b3b022b5c5001351f39bb7
ankeet7x <ankeetkarki30@gmail.com>
2020-08-10 20:18:49 +0545

    Bottom Nav Bar added

---
+++ lib/pages/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'favpage.dart';
+import 'homepage.dart';
+import 'person.dart';
+import 'search.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Widget> _children = [Homee(), Fav(), Search(), Person()];
+  int _currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.menu), title: Text("Title")),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), title: Text("Title")),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), title: Text("Title")),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), title: Text("Title")),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homee extends StatefulWidget {
+  @override
+  _HomeeState createState() => _HomeeState();
+}
+
+class _HomeeState extends State<Homee> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Home"),
+    );
+  }
+}
