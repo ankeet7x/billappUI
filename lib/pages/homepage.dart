93d804a47a811e59c1851f7cc09c670fd2d61c6a
ankeet7x <ankeetkarki30@gmail.com>
2020-08-12 08:31:26 +0545

    YO

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -8,8 +8,49 @@ class Homee extends StatefulWidget {
 class _HomeeState extends State<Homee> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home"),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                appBar(),
+                gap(),
+                Text("March Budget"),
+                Text(
+                  "3500",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+                Text("Remaining Funds"),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
+}
+
+appBar() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      IconButton(
+        icon: Icon(Icons.search),
+        onPressed: () {},
+      ),
+      IconButton(
+        icon: Icon(Icons.settings),
+        onPressed: () {},
+      )
+    ],
+  );
+}
+
+gap() {
+  return SizedBox(
+    height: 10,
+  );
 }
